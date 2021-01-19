4f1524bda0e4a5e85d781dcc87a68dd814c2d885
user <627319180@qq.com>
2021-01-19 22:31:46 +0800

    处理任务状态20210119

--- static/js/taskJs.js
+++ static/js/taskJs.js
@@ -2,10 +2,32 @@
 
 $(document).ready(function () {
     $("#taskVerify tbody").on("click", "button[name='sava']", function () {
-        // alert(1)
+        // alert(1) $("#taskVerify tbody").parent().parent().find("td :eq(2)").val()
         var selec = $(this).parent().parent().find("select option:selected").attr("value");
         console.log("button[name='save'] ;" + selec)
-        $.post("/task/taskVerifyResult/",{"selec":selec},function () {
+        // 时间计算
+        // TODO 工作时间为详细处理
+        var finshiTimeStr = $("button[name='sava']").parent().parent().find("td").eq(3).text()
+        var workTimeStr = $("button[name='sava']").parent().parent().find("td").eq(4).text().split("小时")[0]
+        let finshiTime = new Date(finshiTimeStr)
+        finshiTime.setHours(finshiTime.getHours() + parseInt(workTimeStr))
+        finshiTime.setMinutes(finshiTime.getMinutes() + 30)
+        if (finshiTime < new Date()) {
+            //  继续完成  超时 todo 2021118
+            var timeIn = prompt("输入加急时间，最低1小时，填写整数")
+            console.log(Math.ceil(timeIn))
+            if (!(timeIn < 1 || timeIn > 8) && typeof timeIn === "number") {
+
+            } else {
+                alert("时间为1-8小时")
+                return
+            }
+        } else {
+            //  继续完成  未超时
+            //     $.post("/task/workGoIn/")
+        }
+
+        $.post("/task/taskVerifyResult/", {"selec": selec}, function () {
 
         })
     })
